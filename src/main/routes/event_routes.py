823bdd90fd8d1edb8d6b5660d2eb50c7d9f74f76
unknown <vitor.pereirap8@gmail.com>
2024-04-04 18:35:35 -0300

    feat: Implementing AttendeesHandle with attendee badge route

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
 from src.http_types.http_request import HttpRequest
-event_route_bp = Blueprint('event_route', __name__)
 from src.data.event_handler import EventHandler
 
+event_route_bp = Blueprint('event_route', __name__)
 @event_route_bp.route("/events", methods=['POST'])
 def create_events():
     http_request = HttpRequest(body=request.json)
