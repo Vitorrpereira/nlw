e693ea1c56e4423a271d7b797a75a57f3fb93569
unknown <vitor.pereirap8@gmail.com>
2024-04-04 17:48:02 -0300

    feat:Implementing get event route

--- src/http_types/http_request.py
+++ src/http_types/http_request.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-class HtppRequest:
+class HttpRequest:
     def __init__(self, body: Dict = None, param: Dict = None ) -> None:
         self.body = body
         self.param = param
